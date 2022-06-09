eae5ae60f2fedbc5fdafbda0e63a37573d2bce4c
Koki <kgoudy64@gmail.com>
2022-06-09 16:38:39 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ movieLine.push('oliver');
 
 //adding mulitple items
 movieLine.push('eve', 'barry', 'bruce', 'clark');
+
+//***Removing items
+let barbell = [];
+
+barbell.push(45);
+barbell.push(45);
+barbell.push(25);
+barbell.push(10);
+barbell.push(10);

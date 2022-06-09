308d9cdeef38007acec1157fb103b55b2fed22a0
Koki <kgoudy64@gmail.com>
2022-06-09 16:16:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-let colors = ['rad', 'blue', 'orange', 'yelloww'];
+let movieLine = ['tom', 'nancy'];
+
+//adding to array
+movieLine[2] = 'pablo';
+
+//add to array using push()
+movieLine.push('oliver');
+
+//adding mulitple items
+movieLine.push('eve', 'barry', 'bruce', 'clark');
